91b29171b756ffcc9a98e88afe4a30ee7892a2f1
sunny <sunnylee1102@gmail.com>
2020-05-15 19:27:54 +0900

    Initial Commit

---
+++ KIOM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KIOM
+//
+//  Created by 엔티시스 on 2020/05/15.
+//  Copyright © 2020 ntsys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
